17e40ea989d6883ef4e06dfd80090e6a86966ed3
a.kondrutskiy <a.kondrutskiy@sw-tech.by>
2019-06-21 17:35:34 +0300

    last commit

---
+++ client/build/precache-manifest.33f47b0a6257affdf06b7552f29f60a4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8a65cb36c1dbb76a74500568e0390f0",
+    "url": "/game/index.html"
+  },
+  {
+    "revision": "439e0756e3dc7ea4387f",
+    "url": "/game/static/css/main.5b216ab6.chunk.css"
+  },
+  {
+    "revision": "a8f13d1c3360c1f08c53",
+    "url": "/game/static/js/2.d9d7c35c.chunk.js"
+  },
+  {
+    "revision": "439e0756e3dc7ea4387f",
+    "url": "/game/static/js/main.12502c79.chunk.js"
+  },
+  {
+    "revision": "bab8f84a0f4d6b7ca58b",
+    "url": "/game/static/js/runtime~main.2d0fbc73.js"
+  },
+  {
+    "revision": "58bf8e0bd1dd213bf745e9d748caf175",
+    "url": "/game/static/media/Roboto.58bf8e0b.woff"
+  },
+  {
+    "revision": "481beeb1b84d9e4572ae1dc4af15195b",
+    "url": "/game/static/media/bg.481beeb1.jpg"
+  },
+  {
+    "revision": "8122c5177e5247fbe4e4dbec298dc88d",
+    "url": "/game/static/media/bg.8122c517.jpg"
+  },
+  {
+    "revision": "c1f647636ec45e7aba4c4e30d59b1c78",
+    "url": "/game/static/media/smile.c1f64763.jpg"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.85446b817135530d61ff2453e792736e.js"
+  "/game/precache-manifest.33f47b0a6257affdf06b7552f29f60a4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
